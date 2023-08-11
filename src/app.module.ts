b733c19657b7ec6beb328e1df58c52c5555946d1
Rajeev <scraptorajeev@gmail.com>
2023-08-11 18:16:21 +0530

    Enable DB Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './providers/database/database.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatabaseModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
