40547043f867c06338957638f8fa57c8195b5942
Rajeev <scraptorajeev@gmail.com>
2023-08-12 01:45:25 +0530

    Swagger enabled for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ---- Validation Pipe ------
   app.useGlobalPipes(
     new ValidationPipe({
       forbidUnknownValues: true,
@@ -13,7 +16,22 @@ async function bootstrap() {
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
+
+  // ---- Open API config for Swagger ------
+  const config = new DocumentBuilder()
+    .setTitle('Mars API Services')
+    .setDescription(
+      'Mars API Services. \n For invoking APIs, use Bearer token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibWFyc19kZXYiLCJpYXQiOjE2OTE3ODMxODAsImV4cCI6MTcyMzMxOTE4MH0.H0oDr2URlf4gIY7L064l4l5hjEZJVdNZCockfHvH7Fc',
+    )
+    .setVersion('1.0')
+    .addTag('posts')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   console.log(`Server running on port 127.0.0.1:${port}`);
+  console.log(`Swagger doc running on port 127.0.0.1:${port}/documentation`);
   await app.listen(port);
 }
 bootstrap();
--- src/modules/post/dtos/create-post.dto.ts
+++ src/modules/post/dtos/create-post.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsDefined, IsString, Length } from 'class-validator';
 
 export class CreatePostDTO {
+  @ApiProperty({ default: 'A nice post title' })
   @IsDefined()
   @IsString()
   @Length(1, 255)
-  title: string;
+  readonly title: string;
 
+  @ApiProperty({ default: 'A nice post description' })
   @IsDefined()
   @IsString()
   @Length(1, 1000)
-  description: string;
+  readonly description: string;
 
+  @ApiProperty()
   @IsDefined()
   @IsBoolean()
-  isPublished: boolean;
+  readonly isPublished: boolean;
 }
